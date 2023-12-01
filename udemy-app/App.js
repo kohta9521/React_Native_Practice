dea9fb17e8b02fe2e867bebb1d82c0f65cdcd707
k_95.21 <kohta0421kochi@gmail.com>
2023-12-01 19:19:58 +0900

    add new text

--- udemy-app/App.js
+++ udemy-app/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Hello World!</Text> */}
+      <Text style={[styles.text, styles.textRed]}>kohta first app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +18,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+    backgroundColor: 'black',
+    padding: 10,
+  },
+  textRed: {
+    color: "red",
+  }
 });
